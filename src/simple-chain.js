67ed037ac87b46c450861e694c52c1f458ca9950
katiadeo <niel.0305@yandex.ru>
2021-03-10 00:05:47 +0300

    init: start simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  sequence : [],
+  
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.length;
   },
+      
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (arguments.length == 0) this.sequence.push('( )');
+    else this.sequence.push(`${value}`);
+    return this;
   },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+    
+    removeLink(position) {
+      if (position === '') {
+        this.sequence = [];
+        throw new Error('nonexistent link');
+      }
+      
+      if (isNaN(position)) {
+        this.sequence = [];
+        throw new Error('Not a number');
+      }
+      
+      if (!Number.isInteger(position)) {
+        this.sequence = [];
+        throw new Error('Not an integer');
+      }
+        
+      this.sequence.splice(position - 1, 1);
+      return this;
+    },
+    
+    reverseChain() {
+      this.sequence = this.sequence.reverse();
+      return this;
+    },
+    
+    finishChain() {
+      let finished = `( ${this.sequence.join(' )~~( ')} )`;
+      this.sequence = [];
+      return finished;
+    }
+  };
 
 module.exports = chainMaker;
