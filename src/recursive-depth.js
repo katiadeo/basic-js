7800b0bd5b4577b2d1a726c3207eea324b872d00
katiadeo <niel.0305@yandex.ru>
2021-03-29 21:16:43 +0300

    init: start recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let arrCount = 0; // счетчик
+    let arrDepth = 0; // уровень вложенности
+
+    arr.forEach(char => {
+      if (Array.isArray(char)) { // если элемент массива - массив => 
+        arrCount = this.calculateDepth(char); // присваиваем count функции с этим элементов в виде аргумента
+        if (arrCount > arrDepth) return arrDepth = arrCount;
+      } 
+      
+      if (!Array.isArray(char)) return 1; // если элемент массива - не массив => вложенности нет, вернуть единицу, т к уже есть один массив
+    })
+
+    return arrDepth + 1; // или вернуть вложенность плюс 1
   }
 };
+
+
+// const depthCalc = new DepthCalculator();
+
+// console.log(depthCalc.calculateDepth([1, 2, 3, 4, 5])); // 1
+// console.log(depthCalc.calculateDepth([1, 2, 3, [4, 5]])) //  2
+// console.log(depthCalc.calculateDepth([[[]]])) // 3
